e725189aefb1b72313ae2be13533a34683e9d0f6
SARRA HAYOUNE <35125070+SarraHayoune@users.noreply.github.com>
2020-07-01 00:17:53 -0400

    add comments

--- lupe.py
+++ lupe.py
@@ -5,128 +5,74 @@ import pylab
 import numpy as np
 import matplotlib.pylab as plt
 import readcol
-'''
-r = (G * BH_Mass) / (stars_vel**2)
-G = 6.674e-11
-'''
+import itertools as it
+from itertools import tee
+import pandas as pd
+import warnings
+import decimal
+import statistics
+# Loading files
+Hfiles = readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/highres/cptmarvel.test.orbit')
+Ffiles =  readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/fatso/cptmarvel.fatso.orbit')
 
-#Now I need a code that will load the snapshots(s will stand for )
-Path = "/media/jillian//cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/highres/"
-files = readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/highres/files.list')
-all_files = files[:,0]
+# DATA
+# Convertions
+m_sol= 2.31e15
+print m_sol.units
+l_kpc  = 25000
+m_g = 1.989e33 # Sun mass in gram
+l_cm = 3.086e21 # kpc to cm
+timee = 38.78
+d_timee = 1.22386438e18
+t_square = 1.49784401e36
+         
+# Highes Accretion
+Denergy =( Ffiles[:,13]* m_sol*( l_kpc**2) *m_g *(l_cm**2))/t_square
+Dtime = Ffiles[:,14]*d_timee
+dEdt = Denergy/Dtime
+Time =((Ffiles[:,1])-0.9999999)*timee
+print(Time)
+print(timee)
 
-#Tell where BH is Function
-def findBH(s):
-    BH = s.stars[pynbody.filt.LowPass('tform', 0.0)]
-    return BH
 
-#std stands for standard deviation (velocity dispersion)
-def DispersionVelocity(s):
-    velocity = s.stars['vel']
-    x = np.std(velocity[0])
-    y = np.std(velocity[1])
-    z = np.std(velocity[2])
-    #print(x_direct)
-    #print(y_direct)
-    #print(z_direct)
-    dispersion_velocity = np.sqrt( (x)**2 + (y)**2 + (z)**2)
-    #print("Dispersion velocity: ",dispersion_velocity)
-    return dispersion_velocity
+# Functions
 
-#Need my units to match up so the calculations go correctly
-#Couldn't find a way to convert G, so i converted everything else and then converte back to KPC
-def RadInfluence(s):
-    G = 6.674e-11
-    #G is in m**3 * kg**-1 * s**-2
-    BH = findBH(s)
-    BH_Mass = BH['mass'].in_units('kg')
-    #Kg mtches kg in G
-    stars_vel = DispersionVelocity(s) * 1e3
-    r = (G * BH_Mass) / (stars_vel**2)
-    return r * 3.24e-20*3
+def pair(iterable):
+    "c -> (c0,c1), (c1,c2), (c2, c3), ..." # This function creates ordered pairs 
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
-#Finally converted back to KPC (the conversion is * 3.24e-20)   
-for i in all_files:
-    s = pynbody.load(Path + i)
-    s.physical_units()
-    #Don't forget to center the galaxy with this
-    pynbody.analysis.angmom.faceon(s)
-    BH = findBH(s)
-    BH_pos = BH['pos']
-    BHx = BH_pos[:,0]
-    BHy = BH_pos[:,1]
-    BHz = BH_pos[:,2]
-    BH_position = np.array([BHx[0], BHy[0], BHz[0]])
-    pos_magnitude = np.sqrt((BHx)**2 + (BHy)**2 + (BHz)**2)
-    Mass_Msol = BH['mass']
-    MassBH = Mass_Msol[0]
-    pos_magni = pos_magnitude[0]
-    #print(BH_pos)
-    #dispersion = DispersionVelocity(s)
-    #print(dispersion)
-    #The radius here is an array, we need the center to be an integer
-    radius = RadInfluence(s)
-    radius_influence = radius[0]
-    #print(radius)
-    #BH_pos is a three int array so it will be the center
-    sphere = pynbody.filt.Sphere(radius_influence, cen = BH_position)
-    #print(sphere)
-    stars = s.stars[np.where(s.stars["tform"]>0)]
-    in_sphere = stars[sphere]
-    total_stars = len(in_sphere)
-    #print("Total stars: ",total_stars)
+def float_range(start, stop, step):
+  while start < stop:                 # Float Range function 
+    yield float(start)
+    start += decimal.Decimal(step)
 
-    #This find their velocity
-    velocity = in_sphere['vel']
-    #Now we need to find the velocity of these stars in x,y,z
-    x = np.array([vel[0] for vel in velocity])
-    y = np.array([vel[1] for vel in velocity])
-    z = np.array([vel[2] for vel in velocity])
-    #Now we can find the average of these by dividing by the total
-    vel_answer = np.sqrt((x)**2 + (y)**2 + (z)**2)
-    #Now divide by total number of stars
-    vel_stars_sphere= vel_answer.sum() / total_stars
-    #print("Velocity of the stars in the sphere: ", vel_stars_sphere)
+intervals = pair(float_range(0,2,0.25))
+centers = [(tmin+tmax)/2. for tmin, tmax in intervals]
 
-    # Velocity of the stars with respect of the BH
-    stars_BH_X = x - BH['vel'][:,0]
-    stars_BH_Y = y - BH['vel'][:,1]
-    stars_BH_Z = z - BH['vel'][:,2]
-    stars_xyz = np.sqrt((stars_BH_X)**2 + (stars_BH_Y)**2 +(stars_BH_Z)**2)
-    stars_magnitude = np.sum(stars_xyz)/total_stars
-    #print("Velocity of the stars with respect the BH: ", stars_magnitude)
+def combining(Time,dEdt,intervals):          
+                            # Calculate median valuea given intervals
+    warnings.simplefilter("ignore")
+    out = []
 
-    # VELOCITY OF THE GALAXY
-    galaxy = s['vel']
-    Galaxy_vx = galaxy[:,0]
-    Galaxy_vy = galaxy[:,1]
-    Galaxy_vz = galaxy[:,2]
-    mass = s['mass']
+    for tmin, tmax in intervals:
+        mask = (Time >= tmin) & (Time < tmax)
+        out.append(np.mean(dEdt[mask]))
+    return np.array(out)
 
-    # AVERAGE VELOCITY
-    Av_Vx = sum(Galaxy_vx*mass)/(sum(mass))
-    #print(Av_Vx)
-    Av_Vy = sum(Galaxy_vy*mass)/(sum(mass))
-    #print(Av_Vy)
-    Av_Vz = sum(Galaxy_vz*mass)/(sum(mass))
-    #print(Av_Vz)
-    
-    # BLACK HOLE VELOCITY
-    BH_x = Av_Vx - BH['vel'][:,0]
-    #print(BH_x)
-    BH_y= Av_Vy - BH['vel'][:,1]
-    #print(BH_y)
-    BH_z= Av_Vz - BH['vel'][:,2]
-    #print(BH_z)
+b = len(intervals)
+print(centers)
+print(combining(Time, dEdt, intervals))
 
-    BH_MAGNITUDE= np.sqrt((BH_x)**2 + (BH_y)**2 + (BH_z)**2)
-    BH_VEL = BH_MAGNITUDE[0]
-    #print("Velocity of the black hole: ",BH_MAGNITUDE)
-
-pynbody.analysis.angmom.faceon(s)
-# create an image using  the default bands (i, v, u)
-pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo11Faceon.png')
-
-# create an image using  the default bands (i, v, u)
-pynbody.plot.stars.render(s,width= '5 kpc',plot=True,ret_im=True,filename='halo11Edgeon.png')
+plt.title(" $\Delta$E/$\Delta$t vs Time")
+plt.plot(centers, combining(Time, dEdt, intervals),'ro', label=('FATSO Simulation')) # FATSO
+#plt.scatter(Time, dEdt)
+plt.legend(loc = 'upper right')
+plt.xlabel("Time(Gyrs)")
+plt.ylabel("$\Delta$E/$\Delta$t(Erg/s)")
+plt.yscale('log')
+plt.ylim(10e35,10e38)
+#plt.xlim(0,3)
 plt.show()
+plt.savefig("A_FATSO.png")
