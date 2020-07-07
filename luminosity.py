1923d4e9a8f5f62b3f2f2d520c251de61093d8f3
sarrahayoune <sarah.hayoune@gmail.com>
2020-07-07 02:02:14 -0400

    update time interval

--- luminosity.py
+++ luminosity.py
@@ -14,8 +14,8 @@ import decimal
 import statistics
 
 # Loading files
-Hfiles = readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/highres/cptmarvel.test.orbit')
-Ffiles =  readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.004096/supersample/fatso/cptmarvel.fatso.orbit')
+files = readcol.readcol('/media/jillian/cptmarvel/cptmarvel.cosmo25cmb.4096g5HbwK1BH.orbit
+
 
 # Convertions:
 
@@ -29,15 +29,13 @@ t_square = 1.49784401e36
 m_g = 1.989e33 # Sun mass in gram
 l_cm = 3.086e21 # kpc to cm
 
-
-
          
 # delta energy
-Denergy =( Ffiles[:,13]* m_sol*( l_kpc**2) *m_g *(l_cm**2))/t_square #units here are ergs
+Denergy =( files[:,13]* m_sol*( l_kpc**2) *m_g *(l_cm**2))/t_square #units here are ergs
 #delta time
-Delta_time = Ffiles[:,14]*d_timee
+Delta_time = files[:,14]*d_timee
 dEdt = Denergy/Dtime
-Time =((Ffiles[:,1]))*timee
+Time =((files[:,1]))*timee
 print(Time)
 print(timee)
 
@@ -59,8 +57,10 @@ def float_range(start, stop, step):
   while start < stop:                 # Float Range function 
     yield float(start)
     start += decimal.Decimal(step)
-
-intervals = pair(float_range(0,2,0.25))
+    
+#this is time interval from 0 to 13.8 Gyr with an interval of 0.01 which equals to 10 million years
+intervals = pair(float_range(0,13.8,0.01))
+# tmin,tmax for each time interval
 centers = [(tmin+tmax)/2. for tmin, tmax in intervals]
 
 def combining(Time,dEdt,intervals):          
@@ -69,7 +69,7 @@ def combining(Time,dEdt,intervals):
     out = []
 
     for tmin, tmax in intervals:
-        mask = (Time >= tmin) & (Time < tmax)
+        filter = (Time >= tmin) & (Time < tmax)
         out.append(np.mean(dEdt[mask]))
     return np.array(out)
 
@@ -77,8 +77,9 @@ b = len(intervals)
 print(centers)
 print(combining(Time, dEdt, intervals))
 
+
 plt.title(" $\Delta$E/$\Delta$t vs Time")
-plt.plot(centers, combining(Time, dEdt, intervals),'ro', label=('FATSO Simulation')) # FATSO
+plt.plot(centers, combining(Time, dEdt, intervals),'ro', label=('CptMarvel')) # FATSO
 #plt.scatter(Time, dEdt)
 plt.legend(loc = 'upper right')
 plt.xlabel("Time(Gyrs)")
@@ -86,5 +87,5 @@ plt.ylabel("$\Delta$E/$\Delta$t(Erg/s)")
 plt.yscale('log')
 plt.ylim(10e35,10e38)
 #plt.xlim(0,3)
-plt.show()
-plt.savefig("A_FATSO.png")
+#plt.show()
+plt.savefig("CptMarvel.png")
